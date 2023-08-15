cda5f8fbf59ba587300d7e81fd7342084e4cd3b1
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-08-15 11:14:02 -0600

    Getting Started

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Marco Alonso Rodriguez on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
