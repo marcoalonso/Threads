def8e355100863cb70b5293874ed7f959bc9bf0b
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-08-15 11:20:24 -0600

    Module 1 Project Setup

---
+++ Threads/Core/Authentication/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  Threads
+//
+//  Created by Marco Alonso Rodriguez on 15/08/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
