b454ab9a4fb2b8bfb2ee9e155348ab6e00c6ce12
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-16 14:52:03 -0600

    Module 8 Thread Creation UI

--- Threads/Core/ThreadCreation/View/CreateThreadView.swift
+++ Threads/Core/ThreadCreation/View/CreateThreadView.swift
@@ -8,8 +8,65 @@
 import SwiftUI
 
 struct CreateThreadView: View {
+    @State private var caption = ""
+    @Environment(\.dismiss) private var dismiss
+    
+    
     var body: some View {
-        Text("CreateThreadView")
+        NavigationStack {
+            VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImageView(image: Image("marco"))
+                    
+                    VStack(alignment: .leading, spacing: 4.0) {
+                        Text("marco_alonso_93")
+                            .fontWeight(.semibold)
+                        
+                        TextField("Start a thread ...", text: $caption, axis: .vertical)
+                        
+                    }
+                    .font(.footnote)
+                    
+                    Spacer()
+                    
+                    if !caption.isEmpty {
+                        Button(action: {
+                            caption = ""
+                        }, label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundColor(.gray)
+                        })
+                    }
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                }
+                
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Post") {
+                        
+                    }
+                    .opacity(caption.isEmpty ? 0.5 : 1.0)
+                    .disabled(caption.isEmpty)
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
